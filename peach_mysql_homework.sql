34fce0d0f5c0aee7a43b80ce52e0d710058611db
peach05 <52899180+peach05@users.noreply.github.com>
2019-07-30 15:54:22 +0800

    Add files via upload

---
+++ peach_mysql_homework.sql
@@ -0,0 +1,92 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- 主機： 127.0.0.1
+-- 產生時間： 2019 年 07 月 30 日 09:53
+-- 伺服器版本： 10.3.16-MariaDB
+-- PHP 版本： 7.3.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- 資料庫： `peach_mysql_homework`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `client`
+--
+
+CREATE TABLE `client` (
+  `id` int(11) NOT NULL,
+  `img` text COLLATE utf8_unicode_ci NOT NULL,
+  `tel` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `name` varchar(50) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `product`
+--
+
+CREATE TABLE `product` (
+  `id` int(11) NOT NULL,
+  `img01` text COLLATE utf8_unicode_ci NOT NULL,
+  `img02` text COLLATE utf8_unicode_ci NOT NULL,
+  `img03` text COLLATE utf8_unicode_ci NOT NULL,
+  `img04` text COLLATE utf8_unicode_ci NOT NULL,
+  `img05` text COLLATE utf8_unicode_ci NOT NULL,
+  `img06` text COLLATE utf8_unicode_ci NOT NULL,
+  `img07` text COLLATE utf8_unicode_ci NOT NULL,
+  `img08` text COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `transaction`
+--
+
+CREATE TABLE `transaction` (
+  `id` int(11) NOT NULL,
+  `date` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- 已傾印資料表的索引
+--
+
+--
+-- 資料表索引 `client`
+--
+ALTER TABLE `client`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 資料表索引 `product`
+--
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 資料表索引 `transaction`
+--
+ALTER TABLE `transaction`
+  ADD PRIMARY KEY (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
